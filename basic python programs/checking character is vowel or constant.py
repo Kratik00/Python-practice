4a33d27e321ef3672cf9871ca827704bcd704778
kratik <kratik437@gmail.com>
2026-06-21 04:03:07 +0530

    checking character is constant or vowel

---
+++ basic python programs/checking character is vowel or constant.py
@@ -0,0 +1,11 @@
+#for checking this we create a list containing all the vowels and then we check if the character is present in the list or not
+#Taking input from user
+try:
+    character = input("Enter a character: ")
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    if character in vowels:
+        print("This character is vowel.")
+    else:
+        print("This character is constant.")
+except ValueError:
+    print("Please enter a valid character.")
