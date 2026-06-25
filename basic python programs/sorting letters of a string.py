e019ee5eda3e2587ca957cb1666e0dd373fefd22
kratik <kratik437@gmail.com>
2026-06-25 19:40:02 +0530

    sorting letters of a string

---
+++ basic python programs/sorting letters of a string.py
@@ -0,0 +1,7 @@
+#For sorting of letters we take a string variable and then use two function sorted and join
+
+str = "fgtyhekwtaa"
+letters = sorted(str)
+
+print("".join(letters))
+#we take empty string and join the list of sorted letters
